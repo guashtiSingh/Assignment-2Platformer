2ec083b486911201a5eab0983eef9828987e1352
Guashti Singh <gsing540@my.centennialcollege.ca>
2016-02-26 14:38:58 -0500

    jump force, move force, and camera setup completed

--- Assets/_Scripts/HeroController.cs
+++ Assets/_Scripts/HeroController.cs
@@ -1,38 +1,123 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+//Velocity Range Utility Class
+[System.Serializable]
+public class VelocityRange {
+	//Public Instance Variables
+	public float minimum;
+	public float maximum;
+
+	//Constructor
+	public VelocityRange (float minimum, float maximum) {
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+}
+
 public class HeroController : MonoBehaviour {
+
+	//Public Instance Variables
+	public VelocityRange velocityRange;
+	public float moveForce;
+	public float jumpForce;
+	public Transform groundCheck;
+	public Transform camera;
+
 	// private instance variables
 	private Animator _animator;
 	private float _move;
 	private float _jump;
-
+	private bool _facingRight;
+	private Transform _transform;
+	private Rigidbody2D _rigidBody2D;
+	private bool _isGrounded;
 
 	// Use this for initialization
 	void Start () {
+		//Initialize public instance variables
+		this.velocityRange = new VelocityRange(300f, 30000f);
+
+		//Initialize private instance vairables
+		this._rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+		this._transform = gameObject.GetComponent<Transform> ();
 		this._animator = gameObject.GetComponent<Animator> ();
 		this._move = 0f;
 		this._jump = 0f;
+		this._facingRight = true;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		this._move = Input.GetAxis ("Horizontal");
-		this._jump = Input.GetAxis ("Vertical");
+	void FixedUpdate () {
+		Vector3 currentPosition = new Vector3 (this._transform.position.x, this._transform.position.y, -10f);
+		this.camera.position = currentPosition;
 
-		if (this._move != 0) {
-			//Call the run button
-			this._animator.SetInteger ("AnimState", 1);
+			this._isGrounded = Physics2D.Linecast(this._transform.position, 
+				this.groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+			Debug.DrawLine (this._transform.position, this.groundCheck.position);
+
+			float forceX = 0f;
+			float forceY = 0f;
+
+			// get absolute value of velocity for out gameObject
+			float absVelX = Mathf.Abs(this._rigidBody2D.velocity.x);
+			float absVelY = Mathf.Abs(this._rigidBody2D.velocity.y);
+
+		if (this._isGrounded) {
+			//get a number between -1 and 1 for both horizontal and vertical axes
+			this._move = Input.GetAxis ("Horizontal");
+			this._jump = Input.GetAxis ("Vertical");
+
+			if (this._move != 0) {
+				if (this._move > 0) {
+					//movement force
+					if (absVelX < this.velocityRange.maximum) {
+						forceX = this.moveForce;
+					}
+					this._facingRight = true;
+					this._flip ();
+				} 
+
+				if (this._move < 0) {
+					//movement force
+					if (absVelX < this.velocityRange.maximum) {
+						forceX = -this.moveForce;
+					}
+					this._facingRight = false;
+					this._flip ();
+				}
+				//Call the run button
+				this._animator.SetInteger ("AnimState", 1);
+			} else {
+				//Set default animation state
+				this._animator.SetInteger ("AnimState", 0);
+			}
+
+			if (this._jump > 0) {
+				//jump force
+				if (absVelY < this.velocityRange.maximum) {
+					forceY = this.jumpForce;
+				}
+			}
 		} else {
-			//Set default animation state
-			this._animator.SetInteger ("AnimState", 0);
-		}
-
-		if (this._jump > 0) {
-			//Call the jump button
+			//Set jump animation state
 			this._animator.SetInteger ("AnimState", 2);
 		}
-	}
+			
+			//Apply the forces to the player
+		this._rigidBody2D.AddForce(new Vector2(forceX, forceY));
+		}
+
+		//Private Methods
+		private void _flip () {
+			if (this._facingRight) {
+				this._transform.localScale = new Vector2 (1, 1);
+			} else {
+				this._transform.localScale = new Vector2 (-1, 1);
+			}
+		}
 }
 
 
